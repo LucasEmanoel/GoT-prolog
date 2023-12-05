c6c1aaa1dbecbc43bbbe73c982f49765c25566bc
Lucas Emanoel <lucas2014.barros@gmail.com>
2023-12-05 12:02:01 -0300

    add basic tests

--- test_family.pl
+++ test_family.pl
@@ -8,6 +8,15 @@ test(irma) :-
     irma(arya_stark, sansa_stark),
     \+ irma(daenerys_targaryen, sansa_stark).
 
+test(irmao) :-
+    irmao(robb_stark, sansa_stark),
+    \+ irmao(eddard_stark, aerys_targaryen),
+    \+ irmao(jon_snow, daenerys_targaryen).
+
+test(filhos) :-
+    filhos( eddard_stark, [arya_stark, bran_stark, rickon_stark, robb_stark, sansa_stark]).
+    \+filhos( eddard_stark, [arya_stark, bran_stark, rickon_stark, robb_stark, jon_snow]).
+
 :- end_tests(families).
 
 run_tests :- run_tests(families). 
